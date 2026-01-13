096e134bcb04138c54d7a6c8de3d51cf41bcda8d
Matias <rnmatias@hotmail.com>
2026-01-13 12:08:23 -0300

    style: add scripts `lint:check` and `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # lumina-bible-app
+
 Lumina is a modern, minimalist Bible study platform for deep reading, vibrant highlighting, and personal commentary. Shed light on the Word.
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
 function Home() {
-    return <>
-        <h1>Lumina App</h1>
-        <p>Lumina is a modern study platform designed to bring clarity to Scripture. Featuring intuitive navigation, vibrant highlighting, and personal commentary tools, Lumina empowers readers to dive deeper into the Word. Whether for academic research or daily devotions, shed light on every verse and organize your spiritual insights in one place.</p>
+  return (
+    <>
+      <h1>Lumina App</h1>
+      <p>
+        Lumina is a modern study platform designed to bring clarity to
+        Scripture. Featuring intuitive navigation, vibrant highlighting, and
+        personal commentary tools, Lumina empowers readers to dive deeper into
+        the Word. Whether for academic research or daily devotions, shed light
+        on every verse and organize your spiritual insights in one place.
+      </p>
     </>
+  );
 }
 
 export default Home;
