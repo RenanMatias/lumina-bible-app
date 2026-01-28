2ca2d696ce0550b7b8d596c9aa6ef2e009f28655
Matias <rnmatias@hotmail.com>
2026-01-27 21:57:03 -0300

    feat: create `migrator` model

---
+++ models/migrator.js
@@ -0,0 +1,64 @@
+import { resolve } from "node:path";
+import database from "infra/database.js";
+import { runner } from "node-pg-migrate";
+import { ServiceError } from "infra/errors.js";
+
+const defaultMigrationOptions = {
+  dryRun: true,
+  dir: resolve(process.cwd(), "infra", "migrations"),
+  direction: "up",
+  verbose: true,
+  migrationsTable: "pgmigrations",
+};
+
+async function createClient() {
+  try {
+    const dbClient = await database.getNewClient();
+    return dbClient;
+  } catch (error) {
+    const serviceErrorObject = new ServiceError({
+      message: "Connection error with the database.",
+      cause: error,
+    });
+
+    throw serviceErrorObject;
+  }
+}
+
+async function listPendingMigrations() {
+  const dbClient = await createClient();
+
+  try {
+    const pendingMigrations = await runner({
+      ...defaultMigrationOptions,
+      dbClient,
+    });
+
+    return pendingMigrations;
+  } finally {
+    await dbClient.end();
+  }
+}
+
+async function runPendingMigrations() {
+  const dbClient = await createClient();
+
+  try {
+    const migratedMigrations = await runner({
+      ...defaultMigrationOptions,
+      dbClient,
+      dryRun: false,
+    });
+
+    return migratedMigrations;
+  } finally {
+    await dbClient.end();
+  }
+}
+
+const migrator = {
+  listPendingMigrations,
+  runPendingMigrations,
+};
+
+export default migrator;
