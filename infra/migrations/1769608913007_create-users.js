6f1e608e2519b27a3beb128d57a0ce5992457d82
Matias <rnmatias@hotmail.com>
2026-01-29 18:02:03 -0300

    build: normalize modules with `curso.dev´ project

--- infra/migrations/1769608913007_create-users.js
+++ infra/migrations/1769608913007_create-users.js
@@ -1,4 +1,4 @@
-export const up = (pgm) => {
+exports.up = (pgm) => {
   pgm.createTable("users", {
     id: {
       type: "uuid",
@@ -6,27 +6,27 @@ export const up = (pgm) => {
       default: pgm.func("gen_random_uuid()"),
     },
 
-    // For reference, GitHub limits usernames to max 39 characters
+    // For reference, GitHub limits usernames to 39 characters.
     username: {
       type: "varchar(30)",
       notNull: true,
       unique: true,
     },
 
-    // Why 254 to length? See https://stackoverflow.com/a/1199238
+    // Why 254 in length? https://stackoverflow.com/a/1199238
     email: {
       type: "varchar(254)",
       notNull: true,
       unique: true,
     },
 
-    // Why 60 to length? See https://www.npmjs.com/package/bcrypt#hash-info
+    // Why 60 in length? https://www.npmjs.com/package/bcrypt#hash-info
     password: {
       type: "varchar(60)",
       notNull: true,
     },
 
-    // Why Timestamp with Time Zone? See https://justatheory.com/2012/04/postgres-use-timestamptz/
+    // Why timestamp with timezone? https://justatheory.com/2012/04/postgres-use-timestamptz/
     created_at: {
       type: "timestamptz",
       notNull: true,
@@ -41,4 +41,4 @@ export const up = (pgm) => {
   });
 };
 
-export const down = false;
+exports.down = false;
