cb6dfe9380c21780094d368e4376042e9c3798ad
Matias <rnmatias@hotmail.com>
2026-02-03 18:29:07 -0300

    feat: add `features` column to `users` table

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -32,6 +32,7 @@ describe("POST /api/v1/users", () => {
         id: responseBody.id,
         username: "unique_username",
         email: "unique_email@email.com",
+        features: [],
         password: responseBody.password,
         created_at: responseBody.created_at,
         updated_at: responseBody.updated_at,
