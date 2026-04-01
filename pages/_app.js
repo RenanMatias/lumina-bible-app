eacbe9fa97f5a3de2e825afa8abe4a11268c0d3e
Matias <rnmatias@hotmail.com>
2026-04-01 16:06:33 -0300

    feat: enhance provider with night mode and add layout stack

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,22 @@
 import "@primer/primitives/dist/css/functional/themes/light.css";
 import "@primer/primitives/dist/css/functional/themes/dark.css";
-import "@primer/primitives/dist/css/primitives.css";
-import { ThemeProvider, BaseStyles } from "@primer/react";
+import { ThemeProvider, BaseStyles, Stack } from "@primer/react";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider>
+    <ThemeProvider colorMode="night">
       <BaseStyles>
-        <Component {...pageProps} />
+        <Stack
+          style={{
+            backgroundColor: "var(--bgColor-default)",
+            color: "var(--fgColor-default)",
+            minHeight: "100vh",
+          }}
+        >
+          <Component {...pageProps} />
+        </Stack>
       </BaseStyles>
     </ThemeProvider>
   );
 }
-
 export default MyApp;
