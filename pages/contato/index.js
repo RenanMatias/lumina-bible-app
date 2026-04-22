2c89774e53ead3c8bca7fab228a8f9d2f6b30b72
Matias <rnmatias@hotmail.com>
2026-04-22 16:47:51 -0300

    feat: add `/contato` page

---
+++ pages/contato/index.js
@@ -0,0 +1,14 @@
+import { Stack, Heading } from "@primer/react";
+import { MainTemplate } from "templates/MainTemplate/index.jsx";
+
+export default function StatusPage() {
+  return (
+    <MainTemplate>
+      <Stack>
+        <Heading as="h1" variant="large">
+          Contato
+        </Heading>
+      </Stack>
+    </MainTemplate>
+  );
+}
