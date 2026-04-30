745fb545d36aab41ae10211878c5658e6f40ac25
Matias <rnmatias@hotmail.com>
2026-04-29 21:34:16 -0300

    refactor: add `create:ticket` to anonymous user

--- tests/integration/api/v1/tickets/post.test.js
+++ tests/integration/api/v1/tickets/post.test.js
@@ -9,20 +9,107 @@ beforeAll(async () => {
 
 describe("POST /api/v1/tickets", () => {
   describe("Anonymous user", () => {
-    test("Openning a ticket", async () => {
+    test("Without type", async () => {
       const response = await fetch(`${webserver.origin}/api/v1/tickets`, {
         method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          subject: "Subject of the ticket",
+          message: "Message of the ticket",
+          email: "contact@email.com",
+        }),
       });
 
-      expect(response.status).toBe(403);
+      expect(response.status).toBe(400);
 
       const responseBody = await response.json();
 
       expect(responseBody).toEqual({
-        action: 'Verifique se o seu usuário possui a feature "create:ticket"',
-        message: "Você não possui permissão para executar essa ação.",
-        name: "ForbiddenError",
-        status_code: 403,
+        message: "O campo type é obrigatório. Os valores aceitos são: bug, feature_request ou other.",
+        action: 'Verifique se o campo "type" foi enviado e está com um valor válido (bug, feature_request ou other)',
+        name: "ValidationError",
+        status_code: 400,
+      });
+    });
+
+    test("Without subject", async () => {
+      const response = await fetch(`${webserver.origin}/api/v1/tickets`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          type: "bug",
+          message: "Message of the ticket",
+          email: "contact@email.com",
+        }),
+      });
+
+      expect(response.status).toBe(400);
+
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({
+        message: "O campo subject é obrigatório.",
+        action: 'verifique se o campo "subject".',
+        name: "ValidationError",
+        status_code: 400,
+      });
+    });
+
+    test("Without message", async () => {
+      const response = await fetch(`${webserver.origin}/api/v1/tickets`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          type: "bug",
+          subject: "Subject of the ticket",
+          email: "contact@email.com",
+        }),
+      });
+
+      expect(response.status).toBe(400);
+
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({
+        action: 'verifique se o campo "message".',
+        message: "O campo message é obrigatório.",
+        name: "ValidationError",
+        status_code: 400,
+      });
+    });
+
+    test("valid", async () => {
+      const response = await fetch(`${webserver.origin}/api/v1/tickets`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          type: "bug",
+          subject: "Subject of the ticket",
+          message: "Message of the ticket",
+          email: "contact@email.com",
+        }),
+      });
+
+      expect(response.status).toBe(201);
+
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({
+        id: responseBody.id,
+        number: responseBody.number,
+        title: "Subject of the ticket",
+        body: `**Email do remetente:** contact@email.com\n\n**Mensagem:**\nMessage of the ticket`,
+        labels: ["bug"],
+        state: responseBody.state,
+        url: responseBody.url,
       });
     });
   });
