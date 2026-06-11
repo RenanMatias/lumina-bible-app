f94e4b57ffb2dfd52ab68c0fdbf64f27bb3b0568
Matias <rnmatias@hotmail.com>
2026-06-11 20:33:39 -0300

    feat: improve `api/v1/scripts/books/[book]/[chapter]` endpoint

--- pages/api/v1/scriptures/books/[book]/[chapter]/index.js
+++ pages/api/v1/scriptures/books/[book]/[chapter]/index.js
@@ -9,23 +9,34 @@ export default createRouter()
 
 async function getHandler(request, response) {
   const userTryingToGet = request.context.user;
-  const chapter = request.query.chapter;
+  const bookId = request.query.book;
+  const chapterId = request.query.chapter;
 
   let immersiveReading = false;
   if (userTryingToGet.features.includes("read:scripture:immersive_reading")) {
     immersiveReading = request.query.immersive_reading === "true";
   }
 
-  const chaptersObject = await scripture.findOneChapterById(chapter);
+  const bookObject = await scripture.findOneBookById(bookId);
+  const chaptersObject = await scripture.findOneChapterById(chapterId);
 
-  const pericopesFound = await scripture.findPericopesByChapterId(chapter);
+  delete chaptersObject.book_id;
+  chaptersObject.book = {
+    id: bookObject.id,
+    short_name: bookObject.book,
+    name: bookObject.name,
+    testament: bookObject.testament,
+    version: bookObject.version,
+  };
+
+  const pericopesFound = await scripture.findPericopesByChapterId(chapterId);
 
   for (const pericope of pericopesFound) {
     const versesFound = await scripture.findVersesByPericopeId(pericope.id);
 
     pericope.verses = versesFound.map(({ number, verse }) => {
       verse = scripture.replaceVersePlaceholders(verse, userTryingToGet, immersiveReading);
-      return { [parseInt(number)]: verse };
+      return { number: parseInt(number), text: verse };
     });
   }
 
